2df6e774792e47e8983889d67f6d119de26d4956
Alican Karayelli <alicankarayelli@gmail.com>
2018-02-07 17:39:32 +0300

    Date extension improved

--- CoreUtilFramework.podspec
+++ CoreUtilFramework.podspec
@@ -2,7 +2,7 @@ Pod::Spec.new do |s|
           #1.
           s.name               = "CoreUtilFramework"
           #2.
-          s.version            = "2.0.10"
+          s.version            = "2.0.11"
           #3.  
           s.summary         = "'CoreUtilFramework' is bunch of files that consists util classes for Swift language. Extensions, Delegates, Custom Componenets..."
           #4.
@@ -14,7 +14,7 @@ Pod::Spec.new do |s|
           #7.
           s.platform            = :ios, "9.0"
           #8.
-          s.source              = { :git => "https://github.com/akarayelli/CoreUtilFramework.git", :tag => "2.0.10", :tag => s.version.to_s }
+          s.source              = { :git => "https://github.com/akarayelli/CoreUtilFramework.git", :tag => "2.0.11", :tag => s.version.to_s }
           #9.
           s.source_files     = "CoreUtilFramework", "CoreUtilFramework/**/*.{h,m,swift}"
     end
--- CoreUtilFramework/Extensions/DateExtension.swift
+++ CoreUtilFramework/Extensions/DateExtension.swift
@@ -5,7 +5,7 @@ import Foundation
 // MARK: - Format
 
 public extension Date {
-
+    
     public init?(fromString string: String, format: String) {
         let formatter = SDateFormatter.sharedInstance
         formatter.dateFormat = format
@@ -14,14 +14,14 @@ public extension Date {
         }
         self.init(timeInterval: 0, since: date)
     }
-
+    
     public func toString(dateStyle: DateFormatter.Style = .medium, timeStyle: DateFormatter.Style = .medium) -> String {
         let formatter = SDateFormatter.sharedInstance
         formatter.dateStyle = dateStyle
         formatter.timeStyle = timeStyle
         return formatter.string(from: self as Date)
     }
-
+    
     public func toString(format: String) -> String {
         let formatter = SDateFormatter.sharedInstance
         formatter.dateFormat = format
@@ -29,7 +29,7 @@ public extension Date {
     }
     
     
-    func formattedWith(_ date : String, currentFormat : String, formatToConvert : String) -> String{
+    public static func formattedWith(_ date : String, currentFormat : String, formatToConvert : String) -> String{
         let formatter = DateFormatter()
         formatter.dateFormat = currentFormat
         let dateObject = formatter.date(from: date)
@@ -37,41 +37,100 @@ public extension Date {
         return formatter.string(from: dateObject!)
     }
     
-    func formattedWith(_ format : String) -> String{
+    public func formattedWith(_ format : String) -> String{
         let formatter = DateFormatter()
         formatter.dateFormat = format
         return formatter.string(from: self)
     }
-
-
 }
 
 // MARK: - InBetweenDate
 
 public extension Date {
-
+    
     public func daysInBetweenDate(_ date: Date) -> Double {
         var diff = self.timeIntervalSinceNow - date.timeIntervalSinceNow
         diff = fabs(diff / 86400)
         return diff
     }
-
+    
     public func hoursInBetweenDate(_ date: Date) -> Double {
         var diff = self.timeIntervalSinceNow - date.timeIntervalSinceNow
         diff = fabs(diff / 3600)
         return diff
     }
-
+    
     public func minutesInBetweenDate(_ date: Date) -> Double {
         var diff = self.timeIntervalSinceNow - date.timeIntervalSinceNow
         diff = fabs(diff / 60)
         return diff
     }
-
+    
     public func secondsInBetweenDate(_ date: Date) -> Double {
         var diff = self.timeIntervalSinceNow - date.timeIntervalSinceNow
         diff = fabs(diff)
         return diff
     }
-
 }
+
+
+public extension Date{
+    
+    
+    /// Returns the amount of years from another date
+    public func years(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.year], from: date, to: self).year ?? 0
+    }
+    /// Returns the amount of months from another date
+    public func months(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.month], from: date, to: self).month ?? 0
+    }
+    /// Returns the amount of weeks from another date
+    public func weeks(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.weekOfYear], from: date, to: self).weekOfYear ?? 0
+    }
+    /// Returns the amount of days from another date
+    public func days(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.day], from: date, to: self).day ?? 0
+    }
+    /// Returns the amount of hours from another date
+    public func hours(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.hour], from: date, to: self).hour ?? 0
+    }
+    /// Returns the amount of minutes from another date
+    public func minutes(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.minute], from: date, to: self).minute ?? 0
+    }
+    /// Returns the amount of seconds from another date
+    public func seconds(from date: Date) -> Int {
+        return Calendar.current.dateComponents([.second], from: date, to: self).second ?? 0
+    }
+    /// Returns the a custom time interval description from another date
+    public func offset(from date: Date, offsetType: DateOffsetType) -> Int {
+        
+        if(offsetType == .years){ return years(from: date) }
+        if(offsetType == .months){ return months(from: date) }
+        if(offsetType == .weeks){ return weeks(from: date) }
+        if(offsetType == .days){ return days(from: date) }
+        if(offsetType == .hours){ return hours(from: date) }
+        if(offsetType == .minutes){ return minutes(from: date) }
+        if(offsetType == .seconds){ return seconds(from: date) }
+        
+        return 0
+    }
+    
+}
+
+public enum DateOffsetType: Int {
+    case years
+    case months
+    case weeks
+    case days
+    case hours
+    case minutes
+    case seconds
+    
+}
+
+
+
