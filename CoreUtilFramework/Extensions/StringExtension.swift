dab9b4ba5de0e2702dab33482f2e44e7d92a2c31
Aykut Ersahin <aykutersahin@gmail.com>
2018-04-09 14:15:10 +0300

    valid email check regex fixed

--- CoreUtilFramework/Extensions/StringExtension.swift
+++ CoreUtilFramework/Extensions/StringExtension.swift
@@ -162,7 +162,7 @@ public extension String {
     }
 
     public var isEmail: Bool {
-        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: self)
     }
 
