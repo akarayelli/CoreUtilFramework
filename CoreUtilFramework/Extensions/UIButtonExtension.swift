df8ac3ecbb788e1694551cb3184e8ac1783bd36e
Alican Karayelli <alicankarayelli@gmail.com>
2018-06-28 14:20:49 +0300

    UIButton SetBackgroundColor Extension added!

--- CoreUtilFramework/Extensions/UIButtonExtension.swift
+++ CoreUtilFramework/Extensions/UIButtonExtension.swift
@@ -27,6 +27,17 @@ extension UIButton {
         }
     }
     
+    
+    open func setBackgroundColor(color: UIColor, forState: UIControlState){
+        UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+        UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
+        UIGraphicsGetCurrentContext()!.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    
+        self.setBackgroundImage(colorImage, for: forState)
+    }
+    
 }
 
 
