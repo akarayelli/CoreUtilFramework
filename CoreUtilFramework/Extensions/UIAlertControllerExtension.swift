de46442d04343447a875ec18dbe2d500d5c0bf1f
Eray Alparslan <erayalparslan@yahoo.com>
2020-06-17 11:36:43 +0300

    make alert controller compatible with iPad devices.

--- CoreUtilFramework/Extensions/UIAlertControllerExtension.swift
+++ CoreUtilFramework/Extensions/UIAlertControllerExtension.swift
@@ -4,8 +4,10 @@ import UIKit
 
 public extension UIAlertController {
 
-    static func show(title: String, message: String, cancelTitle: String = "OK") {
+    /// Provide onView parameter to make it compatible with iPad devices.
+    static func show(title: String, message: String, cancelTitle: String = "OK", onView: UIView? = nil) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        if onView != nil { alertController.popoverPresentationController?.sourceView = onView }
         let cancelAction = UIAlertAction(title: cancelTitle, style: .cancel, handler: nil)
         alertController.addAction(cancelAction)
         UIApplication.shared.topViewController()?.present(alertController, animated: true, completion: nil)
